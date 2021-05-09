c6957d4c98de6cd69450588f73132bab5c679d8f
ks251111 <k.11t596@gmail.com>
2021-05-09 17:32:37 +0900

    投稿記事の一覧表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @articles = Article.all
+    @articles = Article.order('created_at DESC')
   end
 
   def new
