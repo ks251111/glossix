db155d4335e5d6e52c5503d812ff4caf5ec318d3
ks251111 <k.11t596@gmail.com>
2021-05-09 13:31:25 +0900

    articlesテーブルの修正

--- db/migrate/20210508095854_create_articles.rb
+++ db/migrate/20210508095854_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     create_table :articles do |t|
       t.string     :title,       null: false
       t.text       :text,        null: false
-      t.integer    :category_id, null: false
       t.references :user,        null: false, foreign_key: true
       t.timestamps
     end
