c07f94a9a4276f2b8b5f7a25701c7c37f750e5ff
ks251111 <k.11t596@gmail.com>
2021-05-10 19:02:59 +0900

    モデルファイルの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  belongs_to :category
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 
   with_options presence: true do
     validates :title
