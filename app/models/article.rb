7a5c58ce2a72b9c4e05bd92944ae350ec63c5c69
ks251111 <k.11t596@gmail.com>
2021-05-10 18:29:37 +0900

    アソシエーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
   has_one_attached :image
 
   with_options presence: true do
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,12 @@ class Category < ActiveHash::Base
     { id: 1, name: '---' },
     { id: 2, name: 'メイク' }
     { id: 3, name: 'スキンケア' }
+    { id: 4, name: 'ボディケア' }
+    { id: 5, name: 'ヘアケア・スタイリング' }
+    { id: 6, name: 'ネイル' }
+    { id: 7, name: 'その他' }
   ]
+
+  include ActiveHash::Associations
+  has_many :articles
 end
