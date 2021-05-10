6814e45ea089416194531f9e2bfadc34488d8066
ks251111 <k.11t596@gmail.com>
2021-05-10 20:40:58 +0900

    commentsコントローラーの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to article_path(@comment.article)
+    else
+      @article = @comment.article
+      @comments = @article.comments
+      render "articles/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
   end
 end
