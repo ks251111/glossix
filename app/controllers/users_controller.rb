8d630b65a12930f5deb8e4e1f1d1dcbe4af3b5eb
ks251111 <k.11t596@gmail.com>
2021-05-11 14:53:39 +0900

    コントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @articles = current_user.article
+    @user = User.find(params[:id])
   end
 end
