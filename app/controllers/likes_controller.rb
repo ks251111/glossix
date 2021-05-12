e3ebeba1b86e8d4caeab6154c03e0d9f6616a155
ks251111 <k.11t596@gmail.com>
2021-05-12 15:30:31 +0900

    createアクションの修正とdestroyアクションの追記

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,11 @@
 class LikesController < ApplicationController
   def create
-    Like.create(user_id: current_user.id, article_id: params[:id])
-    redirect_to articles_path
+    @like = Like.create(user_id: current_user.id, article_id: params[:article_id])
+    redirect_to article_path(@like.article)
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, article_id: params[:article_id]).destroy
+    redirect_to article_path(@like.article)
   end
 end
