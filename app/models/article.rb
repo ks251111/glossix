22deb8c9bb9e96b13c2a0034b254563b66980a74
ks251111 <k.11t596@gmail.com>
2021-05-13 21:32:48 +0900

    searchメソッドの定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,4 +14,12 @@ class Article < ApplicationRecord
   end
 
   validates :category_id, numericality: { other_than: 1 }
+
+  def self.search(search)
+    if search != ""
+      Article.where('title LIKE(?) OR text LIKE(?)', "%#{search}%", "%#{search}%")
+    else
+      Article.all
+    end
+  end
 end
