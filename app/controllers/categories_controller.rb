33feba8a275a9da6c102bdf76f874c0771b22bcb
ks251111 <k.11t596@gmail.com>
2021-05-14 13:59:26 +0900

    showアクションの定義

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+  def show
+    @article = Article.find_by(category_id: params[:id])
+    @articles = Article.where(category_id: params[:id]).order('created_at DESC')
+  end
 end
