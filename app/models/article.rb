2595e37cc1ce8c7715b1ed5a54bf7a2c15d72cd2
ks251111 <k.11t596@gmail.com>
2021-05-16 13:49:57 +0900

    アソシエーションの変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
