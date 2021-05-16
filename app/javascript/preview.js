19e7bfdd3f98db4860138b8389e3501799aef6c9
ks251111 <k.11t596@gmail.com>
2021-05-16 14:06:22 +0900

    2枚目以降の画像投稿の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -4,20 +4,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element");
+      let imageElementNum = document.querySelectorAll('.image-element').length;
 
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
       blobImage.setAttribute('class', 'preview-img');
 
+      const inputHTML = document.createElement('input');
+      inputHTML.setAttribute('id', `article_image_${imageElementNum}`);
+      inputHTML.setAttribute('name', 'article[images][]');
+      inputHTML.setAttribute('type', 'file');
+
       imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
       ImageList.appendChild(imageElement);
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      });
     };
 
     document.getElementById('article_image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
