a7c1991f0e399c1387093bbe91a7fcdbaeaa48a1
ks251111 <k.11t596@gmail.com>
2021-05-16 13:56:48 +0900

    ストロングパラメーターの編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text, :image, :category_id).merge(user_id: current_user.id)
+    params.require(:article).permit(:title, :text, :category_id, images: []).merge(user_id: current_user.id)
   end
 
   def set_article
