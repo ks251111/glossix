71a1dc7a23d694da8d09305a1a858176fb235b71
ks251111 <k.11t596@gmail.com>
2021-05-17 22:01:59 +0900

    talkモデルのテスト

--- spec/factories/talks.rb
+++ spec/factories/talks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :talk do
-    
+    title { Faker::Lorem.sentence }
+    text  { Faker::Lorem.sentence }
+    association :user
   end
 end
--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Talk, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @talk = FactoryBot.build(:talk)
+  end
+
+  describe '掲示板の投稿機能' do
+    context '投稿できるとき' do
+      it 'titleとtextが存在すれば投稿できる' do
+        expect(@talk).to be_valid
+      end
+    end
+
+    context '投稿できないとき' do
+      it 'titleが空では投稿できない' do
+        @talk.title = ''
+        @talk.valid?
+        expect(@talk.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'textが空では投稿できない' do
+        @talk.text = ''
+        @talk.valid?
+        expect(@talk.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'userが紐付いていないと投稿できない' do
+        @talk.user = nil
+        @talk.valid?
+        expect(@talk.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
