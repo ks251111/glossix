2b70214817482e492255b46226033814670cb0a0
ks251111 <k.11t596@gmail.com>
2021-05-17 19:35:05 +0900

    画像設置場所の変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
