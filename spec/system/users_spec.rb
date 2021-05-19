cbc67aab5524ea1eb1c65bfe8e22152709e6de81
ks251111 <k.11t596@gmail.com>
2021-05-19 20:14:55 +0900

    ユーザー新規登録の結合テスト

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # サインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: @user.nickname
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      fill_in 'パスワード(確認)', with: @user.password_confirmation
+      # 添付する画像を定義する
+      image_path = Rails.root.join('public/images/test_image.png')
+      # 画像選択フォームに画像を添付する
+      attach_file('user[image]', image_path, make_visible: true)
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect do
+        click_button "登録する"
+      end.to change(User, :count).by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # サインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: ''
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード', with: ''
+      fill_in 'パスワード(確認)', with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect do
+        click_button "登録する"
+      end.to change(User, :count).by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
+    end
+  end
+end
