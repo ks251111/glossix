c8f74b61743aa6eeb2af530063168512337c94cd
ks251111 <k.11t596@gmail.com>
2021-05-22 19:30:18 +0900

    掲示板詳細表示の結合テスト

--- spec/system/talks_spec.rb
+++ spec/system/talks_spec.rb
@@ -42,3 +42,35 @@ RSpec.describe "掲示板の投稿", type: :system do
     end
   end
 end
+
+RSpec.describe '掲示板詳細', type: :system do
+  before do
+    @talk = FactoryBot.create(:talk)
+  end
+  
+  it 'ログインしたユーザーは掲示板詳細ページに遷移してコメント投稿欄が表示される' do
+    # ログインする
+    sign_in(@talk.user)
+    # 掲示板のトップページに移動する
+    visit talks_path
+    # スレッドの詳細ページに遷移する
+    visit talk_path(@talk)
+    # 詳細ページにスレッドの内容が含まれている
+    expect(page).to have_content("#{@talk.title}")
+    expect(page).to have_content("#{@talk.text}")
+    # コメント用のフォームが存在する
+    expect(page).to have_selector 'form'
+  end
+
+  it 'ログインしていない状態で掲示板詳細ページに遷移できるもののコメント投稿欄が表示されない' do
+    # 掲示板のトップページに移動する
+    visit talks_path
+    # スレッドの詳細ページに遷移する
+    visit talk_path(@talk)
+    # 詳細ページにスレッドの内容が含まれている
+    expect(page).to have_content("#{@talk.title}")
+    expect(page).to have_content("#{@talk.text}")
+    # 「コメントの投稿には新規登録/ログインが必要です」が表示されていることを確認する
+    expect(page).to have_content 'コメントの投稿には新規登録/ログインが必要です'
+  end
+end
