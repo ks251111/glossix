6c3c43154e3d8ca6705bedb836ea69ed7285e7dd
ks251111 <k.11t596@gmail.com>
2021-05-23 18:04:58 +0900

    likeモデルの単体テスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe 'いいね機能' do
+    context 'いいねできるとき' do
+      it 'userとarticleがあればいいねできる' do
+        expect(@like).to be_valid
+      end
+    end
+    context 'いいねできないとき' do
+      it 'userが紐付いていないといいねできない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("User must exist")
+      end
+      it 'articleが紐付いていないといいねできない' do
+        @like.article = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Article must exist")
+      end
+    end
+  end
 end
