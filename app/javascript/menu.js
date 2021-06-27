ac80909b448a43f085774b6636421b9e7a112218
ks251111 <k.11t596@gmail.com>
2021-06-27 17:16:07 +0900

    ハンバーガーメニューの実装

---
+++ app/javascript/menu.js
@@ -0,0 +1,15 @@
+function mainNav() {
+
+  const btn = document.querySelector('.btn-menu');
+  const nav = document.querySelector('.main-nav');
+  btn.addEventListener('click', () => {
+    nav.classList.toggle('open-menu');
+    if (btn.innerHTML === '<i class="fa fa-bars" id="open"></i>') {
+      btn.innerHTML = '<i class="fa fa-times" id="close"></i>';
+    } else {
+      btn.innerHTML = '<i class="fa fa-bars" id="open"></i>';
+    }
+  });
+}
+
+window.addEventListener('load', mainNav)
