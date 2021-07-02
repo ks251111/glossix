ba8a8176cb7a2670b0cd521ca6f7a653e91f89ff
ks251111 <k.11t596@gmail.com>
2021-07-02 23:14:46 +0900

    編集・削除のリンクをドロップダウンメニューに変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ require("../user")
 require("../menu")
 require("../tab")
 require("../accordion")
+require("../dropdown")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
