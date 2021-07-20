a378fe8e107043f89ac1dbb262e2f3381a1d2d1d
ks251111 <k.11t596@gmail.com>
2021-07-20 16:35:46 +0900

    コードの整形

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    if @comment.valid? 
+    if @comment.valid?
       @comment.save
       redirect_to article_path(@comment.article)
     else
@@ -12,6 +12,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
   end
