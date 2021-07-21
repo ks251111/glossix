58e50f7a56e69adcbe490dfeef91015284751d72
ks251111 <k.11t596@gmail.com>
2021-07-21 20:32:23 +0900

    Q&Aの結合テストの修正

--- spec/system/talks_spec.rb
+++ spec/system/talks_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "掲示板の投稿", type: :system do
+RSpec.describe "Q&Aの投稿", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @talk_title = Faker::Lorem.sentence
     @talk_text = Faker::Lorem.sentence
   end
 
-  context '掲示板の投稿ができるとき' do
+  context 'Q&Aの投稿ができるとき' do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
       sign_in(@user)
-      # 掲示板のトップページに遷移する
+      # Q&Aのトップページに遷移する
       visit talks_path
       # 新規投稿ページへのボタンがあることを確認する
       expect(page).to have_content('質問・相談する')
@@ -24,74 +24,76 @@ RSpec.describe "掲示板の投稿", type: :system do
       expect do
         click_button "投稿する"
       end.to change(Talk, :count).by(1)
-      # 掲示板のトップページに遷移することを確認する
+      # Q&Aのトップページに遷移することを確認する
       expect(current_path).to eq(talks_path)
-      # 掲示板のトップページには先ほど投稿した内容のスレッドが存在することを確認する(タイトル)
+      # Q&Aのトップページには先ほど投稿した内容が存在することを確認する(タイトル)
       expect(page).to have_content(@talk_title)
-      # 掲示板のトップページには先ほど投稿した内容のスレッドが存在することを確認する(50文字以内のテキスト)
-      expect(page).to have_content(@talk_text.truncate(50))
+      # Q&Aのトップページには先ほど投稿した内容が存在することを確認する(テキスト)
+      expect(page).to have_content(@talk_text)
     end
   end
 
-  context '掲示板に投稿ができないとき' do
+  context 'Q&Aに投稿ができないとき' do
     it 'ログインしていないと新規投稿ページに遷移できない' do
-      # 掲示板のトップページに遷移する
+      # Q&Aのトップページに遷移する
       visit talks_path
       # 新規投稿ページへのボタンを押す
-      click_on ('質問・相談する')
+      click_on('質問・相談する')
       # ログインページへ遷移したことを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
 end
 
-RSpec.describe '掲示板詳細', type: :system do
+RSpec.describe 'Q&A詳細', type: :system do
   before do
     @talk = FactoryBot.create(:talk)
   end
-  
-  it 'ログインしたユーザーは掲示板詳細ページに遷移して回答用のフォームが表示される' do
+
+  it 'ログインしたユーザーはQ&A詳細ページに遷移して回答用のフォームが表示される' do
     # ログインする
     sign_in(@talk.user)
-    # 掲示板のトップページに移動する
+    # Q&Aのトップページに移動する
     visit talks_path
-    # スレッドの詳細ページに遷移する
+    # 質問の詳細ページに遷移する
     visit talk_path(@talk)
-    # 詳細ページにスレッドの内容が含まれている
-    expect(page).to have_content("#{@talk.title}")
-    expect(page).to have_content("#{@talk.text}")
+    # 詳細ページに質問の内容が含まれている
+    expect(page).to have_content(@talk.title)
+    expect(page).to have_content(@talk.text)
     # 回答用のフォームが存在する
     expect(page).to have_selector 'form'
   end
 
-  it 'ログインしていない状態で掲示板詳細ページに遷移できるものの回答用のフォームが表示されない' do
-    # 掲示板のトップページに移動する
+  it 'ログインしていない状態でQ&A詳細ページに遷移できるものの回答用のフォームが表示されない' do
+    # Q&Aのトップページに移動する
     visit talks_path
-    # スレッドの詳細ページに遷移する
+    # 質問の詳細ページに遷移する
     visit talk_path(@talk)
-    # 詳細ページにスレッドの内容が含まれている
-    expect(page).to have_content("#{@talk.title}")
-    expect(page).to have_content("#{@talk.text}")
+    # 詳細ページに質問の内容が含まれている
+    expect(page).to have_content(@talk.title)
+    expect(page).to have_content(@talk.text)
     # 「回答には新規登録/ログインが必要です」が表示されていることを確認する
     expect(page).to have_content '回答には新規登録/ログインが必要です'
   end
 end
 
-RSpec.describe '掲示板編集', type: :system do
+RSpec.describe 'Q&A編集', type: :system do
   before do
     @talk1 = FactoryBot.create(:talk)
     @talk2 = FactoryBot.create(:talk)
   end
 
-  context 'スレッドの編集ができるとき' do
-    it 'ログインしたユーザーは自分が投稿したスレッドの編集ができる' do
+  context '質問の編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿した質問の編集ができる' do
       # talk1を投稿したユーザーでログインする
       sign_in(@talk1.user)
-      # 掲示板のトップページに移動する
+      # Q&Aのトップページに移動する
       visit talks_path
       # talk1の詳細ページに遷移する
       visit talk_path(@talk1)
-      # talk1の詳細ページに「編集」へのリンクがあることを確認する
+      # 「･･･」のボタンをクリックする
+      find('#dropdown').click
+      # 「編集」へのリンクがあることを確認する
       expect(page).to have_link '編集', href: edit_talk_path(@talk1)
       # 編集ページへ遷移する
       visit edit_talk_path(@talk1)
@@ -111,93 +113,94 @@ RSpec.describe '掲示板編集', type: :system do
       end.to change(Talk, :count).by(0)
       # talk1の詳細ページに遷移することを確認する
       expect(current_path).to eq(talk_path(@talk1))
-      # 詳細ページには先ほど変更した内容のスレッドが存在することを確認する(タイトル)
+      # 詳細ページには先ほど変更した内容の質問が存在することを確認する(タイトル)
       expect(page).to have_content("#{@talk1.title}+編集したタイトル")
-      # 詳細ページには先ほど変更した内容のスレッドが存在することを確認する(テキスト)
+      # 詳細ページには先ほど変更した内容の質問が存在することを確認する(テキスト)
       expect(page).to have_content("#{@talk1.text}+編集したテキスト")
     end
   end
 
-  context 'スレッドの編集ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿したスレッドの編集画面には遷移できない' do
+  context '質問の編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿した質問の編集画面には遷移できない' do
       # talk1を投稿したユーザーでログインする
       sign_in(@talk1.user)
-      # 掲示板トップページに移動する
+      # Q&Aトップページに移動する
       visit talks_path
       # talk2の詳細ページに遷移する
       visit talk_path(@talk2)
-      # talk2の詳細ページに「編集」へのリンクがないことを確認する
-      expect(page).to have_no_link '編集', href: edit_talk_path(@talk2)
+      # talk2の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
     end
-    it 'ログインしていないとスレッドの編集画面には遷移できない' do
-      # 掲示板トップページに移動する
+    it 'ログインしていないと質問の編集画面には遷移できない' do
+      # Q&Aトップページに移動する
       visit talks_path
       # talk1の詳細ページに遷移する
       visit talk_path(@talk1)
-      # talk1の詳細ページに「編集」へのリンクがないことを確認する
-      expect(page).to have_no_link '編集', href: edit_talk_path(@talk1)
-      # 掲示板トップページに移動する
+      # talk1の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
+      # Q&Aトップページに移動する
       visit talks_path
       # talk2の詳細ページに遷移する
       visit talk_path(@talk2)
-      # talk2の詳細ページに「編集」へのリンクがないことを確認する
-      expect(page).to have_no_link '編集', href: edit_talk_path(@talk2)
+      # talk2の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
     end
   end
 end
 
-RSpec.describe '掲示板削除', type: :system do
+RSpec.describe 'Q&A削除', type: :system do
   before do
     @talk1 = FactoryBot.create(:talk)
     @talk2 = FactoryBot.create(:talk)
   end
 
-  context 'スレッド削除ができるとき' do
-    it 'ログインしたユーザーは自らが投稿したスレッドの削除ができる' do
+  context '質問の削除ができるとき' do
+    it 'ログインしたユーザーは自らが投稿した質問の削除ができる' do
       # talk1を投稿したユーザーでログインする
       sign_in(@talk1.user)
-      # 掲示板トップページへ遷移する
+      # Q&Aトップページへ遷移する
       visit talks_path
       # talk1の詳細ページへ遷移する
       visit talk_path(@talk1)
-      # talk1の詳細ページに「削除」へのリンクがあることを確認する
+      # 「･･･」のボタンをクリックする
+      find('#dropdown').click
+      # 「削除」へのリンクがあることを確認する
       expect(page).to have_link '削除', href: talk_path(@talk1)
       # 投稿を削除するとレコードの数が1減ることを確認する
       expect {
         find_link('削除', href: talk_path(@talk1)).click
       }.to change { Talk.count }.by(-1)
-      # 掲示板トップページに遷移したことを確認する
+      # Q&Aトップページに遷移したことを確認する
       expect(current_path).to eq(talks_path)
       # トップページにはtalk1の内容が存在しないことを確認する(タイトル)
-      expect(page).to have_no_content("#{@talk1.title}")
+      expect(page).to have_no_content(@talk1.title)
     end
   end
 
-  context 'スレッド削除ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿したスレッドの削除ができない' do
+  context '質問の削除ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿した質問の削除ができない' do
       # talk1を投稿したユーザーでログインする
       sign_in(@talk1.user)
-      # 掲示板トップページに遷移する
+      # Q&Aトップページに遷移する
       visit talks_path
       # talk2の詳細ページへ遷移する
       visit talk_path(@talk2)
-      # talk2の詳細ページに「削除」へのリンクがないことを確認する
-      expect(page).to have_no_link '削除', href: talk_path(@talk2)
-
+      # talk2の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
     end
-    it 'ログインしていないとスレッドの削除ボタンがない' do
-      # 掲示板トップページに遷移する
+    it 'ログインしていないと質問の削除ボタンがない' do
+      # Q&Aトップページに遷移する
       visit talks_path
       # talk1の詳細ページに遷移する
       visit talk_path(@talk1)
-      # talk1の詳細ページに「削除」へのリンクがないことを確認する
-      expect(page).to have_no_link '削除', href: talk_path(@talk1)
-      # 掲示板トップページに遷移する
+      # talk1の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
+      # Q&Aトップページに遷移する
       visit talks_path
       # talk2の詳細ページに遷移する
       visit talk_path(@talk2)
-      # talk2の詳細ページに「削除」へのリンクがないことを確認する
-      expect(page).to have_no_link '削除', href: talk_path(@talk2)
+      # talk2の詳細ページに「･･･」のボタンがないことを確認する
+      expect(page).to have_no_selector('#dropdown')
     end
   end
 end
